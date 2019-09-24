1446d34b78dc94d9f58d04981030770b55a19ea9
KubaKajderowicz <2115394@talnet.nl>
2019-09-24 15:55:55 +0200

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,11 @@
+<?php
+?>
+
+<html>
+    <body>
+        <form>
+            <input type="email" placeholder="Vul in je email adress">
+            <input type="button" value="verstuur">
+        </form>
+    </body>
+</html>
